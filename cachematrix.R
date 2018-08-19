cd4237cf1fea53d5866133a3a6e1342b26bb21b6
TravisJones01 <41902827+TravisJones01@users.noreply.github.com>
2018-08-19 18:31:45 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## First I had to learn what an inverse matrix does - see https://www.mathsisfun.com/algebra/matrix-inverse.html if you are interested
+##First function takes, gets, and sets a matrix and inverse matrix.
+##The second checks the first for NULL; if NULL calculates by pulling matrix from first function; 
+##if not null returns inverse from first function
 
-## Write a short comment describing this function
+## makeCacheMatrix takes, gets, and sets a matrix and inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        InvMatrix <- NULL       
+        SetMatrix <- function(y) {
+                x <<- y
+                InvMatrix <<- NULL
+        }
+        GetMatrix <- function() x
+        SetInverse <- function(inverse) InvMatrix <<- inverse
+        GetInverse <- function() InvMatrix      
+        list(SetMatrix = SetMatrix, GetMatrix = GetMatrix, 
+             SetInverse = SetInverse, GetInverse = GetInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks the first for NULL; if NULL calculates by pulling matrix from first function; 
+##if not null returns inverse from first function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        InvMatrix <- x$GetInverse()
+        if(!is.null(InvMatrix)) {
+                message("Getting cached data")
+                return(InvMatrix)
+        }
+        data <- x$GetMatrix()
+        InvMatrix <- solve(data, ...)
+        x$SetInverse(InvMatrix)
+        InvMatrix
 }
+
